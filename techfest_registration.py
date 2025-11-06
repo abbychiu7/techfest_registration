da97bd45d3726c402dcd2eb4aba3c58a038bfdd5
Abby Chiu <aprilabegail.chiu@benilde.edu.ph>
2025-11-06 08:54:21 +0800

    Implemented participant registration

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,3 +1,15 @@
 # TechFest Registration System
 # Author: April Abegail B. Chiu
 # Section: APPDAET BTCS1
+
+print("Welcome to SMIT TechFest!")
+print("Organized by April Abegail B. Chiu of APPDAET BTCS1")
+
+try:
+    num_participants = int(input("How many participants will register? "))
+    if num_participants <= 0:
+        print("Invalid number of participants.")
+        exit()
+except ValueError:
+    print("Invalid input. Please enter a number.")
+    exit()
